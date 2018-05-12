fcab50d7f2fbf9b2ee04959563c6e626f5fdbb64
Renjith <renjith@sayonetech.com>
2018-05-12 21:36:01 +0530

    migrations

--- models/User.go
+++ models/User.go
@@ -52,3 +52,15 @@ func (u *User) checkPassword(password string) error {
 //https://github.com/gothinkster/golang-gin-realworld-example-app/blob/master/users/models.go
 //https://medium.com/@jcox250/password-hash-salt-using-golang-b041dc94cb72
 //https://github.com/gin-contrib
+//https://github.com/vsouza/go-gin-boilerplate/tree/master/middlewares\
+//https://github.com/george518/PPGo_Api_Demo_Gin/tree/master/routers
+//https://github.com/gothinkster/golang-gin-realworld-example-app
+// swagger & user https://github.com/EDDYCJY/go-gin-example
+//https://github.com/night-codes/summer
+//https://github.com/szuecs/gin-gomonitor
+//https://github.com/sbecker/gin-api-demo
+//https://github.com/acrosson/gingorm/tree/master/db
+//https://github.com/aviddiviner/gin-limit
+//https://github.com/Luncher/go-rest
+//https://github.com/nightlegend/apigateway
+//https://github.com/rageix/ginAuth
