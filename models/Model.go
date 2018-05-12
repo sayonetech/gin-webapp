2d364e094e447404fb06e9e0185a7a442b722e87
Renjith <renjith@sayonetech.com>
2018-05-12 12:50:21 +0530

    migrations

--- models/Model.go
+++ models/Model.go
@@ -12,7 +12,8 @@ var Model *gorm.DB
 // Read more about init() https://stackoverflow.com/questions/17733220/go-package-initialization
 func init() {
 	var err error
-	Model, err := gorm.Open("postgres", "host="+config.GetEnv().HOST+" port="+config.GetEnv().DATABASE_PORT+" user="+config.GetEnv().DATABASE_USERNAME+" dbname="+config.GetEnv().DATABASE_NAME+" password="+config.GetEnv().DATABASE_PASSWORD)
+
+	Model, err = gorm.Open("postgres", "host="+config.GetEnv().HOST+" port="+config.GetEnv().DATABASE_PORT+" user="+config.GetEnv().DATABASE_USERNAME+" dbname="+config.GetEnv().DATABASE_NAME+" password="+config.GetEnv().DATABASE_PASSWORD+" sslmode=disable")
 
 	if err != nil {
 		panic(err)
