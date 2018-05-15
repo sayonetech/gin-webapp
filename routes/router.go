a2fd6cad1b9db35a554d3144e74aba890bd0e391
Renjith <renjith@sayonetech.com>
2018-05-15 14:06:04 +0530

    swagger doc

--- routes/router.go
+++ routes/router.go
@@ -7,12 +7,15 @@ import (
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
 	// proxy "github.com/chenhg5/gin-reverseproxy"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 //InitRouter Initialise router
 func InitRouter() *gin.Engine {
 	route := gin.New()
 
+	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	if config.GetEnv().DEBUG {
 		route.Use(gin.Logger()) // Used in development mode, console print request records
 		pprof.Register(route)   // Performance Analysis Tool
