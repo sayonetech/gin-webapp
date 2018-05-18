941ce36268bf485615e9062f1ee3c7b281355c60
Renjith <renjith@sayonetech.com>
2018-05-18 11:45:15 +0530

    auth handler

--- middleware/session/auth.go
+++ middleware/session/auth.go
@@ -3,8 +3,8 @@ package session
 // Authorizer structures contain the store of user session cookies a reference
 // to a backend storage system.
 type Authorizer struct {
-	session string //redis session TODO
-	backend string //auth backend TODO
+	session string      //redis session TODO
+	backend AuthBackend //auth backend TODO
 }
 
 //todo https://github.com/apexskier/httpauth/blob/master/auth.go
--- middleware/session/backend.go
+++ middleware/session/backend.go
@@ -1,3 +1,14 @@
 package session
 
+import "beco/models"
+
 //https://github.com/apexskier/httpauth/blob/master/auth.go
+
+// The AuthBackend interface defines a set of methods an AuthBackend must
+// implement.
+type AuthBackend interface {
+	SaveUser(u models.User) error
+	User(id int) (user models.User, e error)
+	DeleteUser(id int) error
+	Close()
+}
