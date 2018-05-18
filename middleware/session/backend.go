b4cd72bfc9d641ac9a4eed938749f152b5673a2d
Renjith <renjith@sayonetech.com>
2018-05-18 12:00:42 +0530

    auth handler

--- middleware/session/backend.go
+++ middleware/session/backend.go
@@ -1,14 +1,45 @@
 package session
 
-import "beco/models"
+import (
+	"go-webapp/models"
+
+	"github.com/jinzhu/gorm"
+)
 
 //https://github.com/apexskier/httpauth/blob/master/auth.go
 
-// The AuthBackend interface defines a set of methods an AuthBackend must
-// implement.
+// The AuthBackend interface defines a set of methods an AuthBackend must implement.
 type AuthBackend interface {
 	SaveUser(u models.User) error
 	User(id int) (user models.User, e error)
 	DeleteUser(id int) error
-	Close()
+}
+
+//Backend Driver
+type Backend struct {
+	driverName string
+	db         *gorm.DB
+}
+
+//NewBackend initilise the backend object
+func NewBackend() (b Backend) {
+	b.driverName = "postgres"
+	b.db = models.Model
+	return b
+}
+
+// User returns the user with the given usedID. Error is set to
+// ErrMissingUser if user is not found.
+func (b Backend) User(id int) (user models.User, e error) {
+	return user, nil
+}
+
+// SaveUser adds a new user
+func (b Backend) SaveUser(u models.User) error {
+	return nil
+}
+
+// DeleteUser removes a user, raising ErrDeleteNull if that user was missing.
+func (b Backend) DeleteUser(id int) error {
+	return nil
 }
